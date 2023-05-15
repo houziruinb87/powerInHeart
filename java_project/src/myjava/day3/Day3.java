3570171527d274774c5230028c928d21e032f055
houzirui <houzirui@babytree-inc.com>
2023-05-15 14:46:57 +0800

    updtate:快速排序

--- java_project/src/myjava/day3/Day3.java
+++ java_project/src/myjava/day3/Day3.java
@@ -1,44 +1,98 @@
 package myjava.day3;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
- * 1.数组中随机挑一个数,把小于他的放于左边,大于的放于右边,等于他的放到中间要求O(n)  荷兰旗问题
- *
+ * 1.数组中随机挑一个数,把小于他的放于左边,大于的放于右边,等于他的放到中间要求O(n)  荷兰旗问题  也称为分层过程partition
+ * <p>
+ * 2.快速排序如果从末尾取数进行分层时间复杂度为O(N^2)如果随机取数分层时间复杂度为O(NlogN)
  */
 public class Day3 {
+    public static int[] sArray;
 
-    public static int[] smallLeftBigRight(int[] array, int numPosition){
-        int num = array[numPosition];
-        System.out.println("需要被比较的数字为"+num);
+    /**
+     * 返回左索引和右索引
+     *
+     * @param array
+     * @return
+     */
+    public static int[] partition(int[] array, int leftPosition, int rightPosition) {
 
-        int leftPosition=0;
-        int rightPosition=array.length-1;
-        int currentPosition=0;
-        while ( currentPosition <= rightPosition){
-            if(array[currentPosition]<num){
-                swap(array,leftPosition,currentPosition);
+
+//        System.out.println("左索引为" + leftPosition);
+//        System.out.println("右索引为" + rightPosition);
+//        int numPosition = rightPosition;
+//        if (rightPosition > 0) {
+//
+//            numPosition = (((new Random().nextInt(rightPosition))) + leftPosition);
+//            if(numPosition>=rightPosition){
+//                numPosition = rightPosition;
+//            }
+//
+//        }
+//        System.out.println("随机数索引为" + numPosition);
+        int num = array[rightPosition];
+//        System.out.println("随机数索引对应的值为" + num);
+//交换随机索引位置的数与最后一个位置
+//        swap(array,array[numPosition],array[rightPosition] );
+
+        int currentPosition = leftPosition;
+        while (currentPosition <= rightPosition) {
+            if (array[currentPosition] < num) {
+                swap(array, leftPosition, currentPosition);
                 leftPosition++;
                 currentPosition++;
-            } else if (array[currentPosition]==num){
+            } else if (array[currentPosition] == num) {
                 currentPosition++;
-            } else if(array[currentPosition]>num){
-                swap(array,currentPosition,rightPosition);
+            } else if (array[currentPosition] > num) {
+                swap(array, currentPosition, rightPosition);
                 rightPosition--;
             }
         }
-        return array;
+        return new int[]{leftPosition, rightPosition};
     }
 
     /**
      *
      */
     public static void swap(int[] array, int i, int j) {
-        if(i==j){
+        if (i == j) {
             return;
         }
         array[i] = array[i] ^ array[j];
         array[j] = array[i] ^ array[j];
         array[i] = array[i] ^ array[j];
+    }
+
+//    public int[] quickSort(int array) {
+//
+//    }
+
+    public static int[] callByOutQuickSort(int[] array, int leftPosition, int rightPosition) {
+        sArray = array;
+        quickSortByPartition(sArray, leftPosition, rightPosition);
+        return sArray;
+    }
+
+    /**
+     * 快拍分层
+     *
+     * @param array
+     * @param leftPosition
+     * @param rightPosition
+     */
+    public static void quickSortByPartition(int[] array, int leftPosition, int rightPosition) {
+      if(leftPosition<rightPosition){
+          int  numPosition = (((new Random().nextInt(rightPosition-leftPosition+1))) + leftPosition);
+          System.out.println("随机索引为" + numPosition);
+          swap(array,numPosition,rightPosition);
+          int[] partition = partition(array, leftPosition, rightPosition);
+//          if (partition[0] <=array.length-1 && partition[1] >=0) {
+              quickSortByPartition(array, leftPosition, partition[0] - 1);
+              quickSortByPartition(array, partition[1] + 1, rightPosition);
+//          }
+      }
+
     }
 }
--- java_project/src/myjava/day3/MainDay3.java
+++ java_project/src/myjava/day3/MainDay3.java
@@ -10,7 +10,8 @@ public class MainDay3 {
  public static    int[] array={1,52,5,234,23,1324,876,8678,5,67,456,345,345,34,56,4526,5,54,3425,435,45,34,52,1,4,4,56,67,457,56,34,5,3456};
 
     public static void main(String[] args) {
-       System.out.println(Arrays.toString(Day3.smallLeftBigRight(array,1)));
+//       System.out.println(Arrays.toString(Day3.partition(array,0, array.length-1)));
+       System.out.println(Arrays.toString(Day3.callByOutQuickSort(array,0,( array.length)-1)));
 
     }
 
