de134a7dbe1a7948da6343aec8454ef070d6db5e
houzirui <houzirui@babytree-inc.com>
2023-05-26 17:15:23 +0800

    update:添加链表是否为环判断,核心是快慢指针问题,首尾相接的环,第一次相遇的点一定是头结点.如果不是首尾相接,需要让快慢指针再次以相同速度,让快指针回到头结点再次相遇的点.

--- java_project/src/myjava/day8/Day8.java
+++ java_project/src/myjava/day8/Day8.java
@@ -1,20 +1,36 @@
 package myjava.day8;
 
-class Node{
+import java.util.Stack;
+
+/**
+ * 链表问题,一般先判断是否为环形链表
+ * 还需要再判断这个环形链表是否为首尾相接
+ * 从而区分找到入环的节点
+ */
+class Node {
     public int value;
     public Node next;
     public Node last;
-    Node(int value){this.value = value;}
+
+    Node(int value) {
+        this.value = value;
+    }
 }
+
+/**
+ * 1. 逆转单向双向链表
+ * 2. 打印两个链表公共部分
+ * 3. 判断链表是否为回文结构
+ */
 public class Day8 {
-    public static Node createSingleNodeByValue(int size){
-        Node head =null;
-        Node current  = null;
-        for(int i = 0; i<size;i++){
-            if(head == null){
+    public static Node createSingleNodeByValue(int size) {
+        Node head = null;
+        Node current = null;
+        for (int i = 0; i < size; i++) {
+            if (head == null) {
                 head = new Node(i);
                 current = head;
-            }else {
+            } else {
                 Node node = new Node(i);
                 current.next = node;
                 current = node;
@@ -23,13 +39,14 @@ public class Day8 {
         }
         return head;
     }
-    public static void printNode(Node node){
-        Node currentNode = node;
-        System.out.println(currentNode.value+"--current="+node+"--last="+node.last+"---next="+node.next);
 
-        while (currentNode.next!=null){
+    public static void printNode(Node node) {
+        Node currentNode = node;
+        System.out.println(currentNode.value + "--current=" + node + "--last=" + node.last + "---next=" + node.next);
+
+        while (currentNode.next != null) {
             currentNode = currentNode.next;
-            System.out.println(currentNode.value+"--current="+currentNode+"--last="+currentNode.last+"---next="+currentNode.next);
+            System.out.println(currentNode.value + "--current=" + currentNode + "--last=" + currentNode.last + "---next=" + currentNode.next);
 
         }
         System.out.println("-----------------------------------");
@@ -38,26 +55,27 @@ public class Day8 {
 
     /**
      * 逆转链表
+     *
      * @param node
      * @return
      */
-    public static Node reverseNodeList(Node node){
-        if(node != null && node.next!=null){
-           Node pre = null;
-           Node current = node;
-           Node curNext;
-           Node newHead = null;
-           while (current!=null){
-               if(current.next==null){
-                   newHead = current;
-               }
-               curNext = current.next;
-               current.next = pre;
-               pre = current;
-               current = curNext;
-           }
+    public static Node reverseNodeList(Node node) {
+        if (node != null && node.next != null) {
+            Node pre = null;
+            Node current = node;
+            Node curNext;
+            Node newHead = null;
+            while (current != null) {
+                if (current.next == null) {
+                    newHead = current;
+                }
+                curNext = current.next;
+                current.next = pre;
+                pre = current;
+                current = curNext;
+            }
             return newHead;
-        }else {
+        } else {
             return node;
         }
     }
@@ -65,21 +83,22 @@ public class Day8 {
 
     /**
      * 构建双向链表
+     *
      * @param doubleNodeListSize
      * @return
      */
-    public static Node createDoubleNodeByValue(int doubleNodeListSize){
+    public static Node createDoubleNodeByValue(int doubleNodeListSize) {
         Node head = null;
         Node pre = null;
-        Node current =null;
-        for(int i =0;i<doubleNodeListSize;i++){
+        Node current = null;
+        for (int i = 0; i < doubleNodeListSize; i++) {
 
             Node newNode = new Node(i);
             //头知道上一个不知道下一个
-            if(head == null){
+            if (head == null) {
                 head = newNode;
                 current = head;
-            }else {
+            } else {
                 current.next = newNode;
                 current = newNode;
 
@@ -92,17 +111,18 @@ public class Day8 {
 
     /**
      * 逆转双向链表
+     *
      * @param node
      * @return
      */
-    public  static Node reversDoubleNodeList(Node node){
+    public static Node reversDoubleNodeList(Node node) {
         Node head = null;
         Node pre = null;
         Node current = node;
-        Node curNext ;
-        while (current!=null){
+        Node curNext;
+        while (current != null) {
             //没有下一个
-            if(current.next==null){
+            if (current.next == null) {
                 head = current;
             }
             curNext = current.next;
@@ -111,6 +131,168 @@ public class Day8 {
             pre = current;
             current = curNext;
         }
-        return  head;
+        return head;
+    }
+
+    //1 2 3  1224  123345
+    public static boolean isNodeListIsHuiWen(Node node) {
+        Node slowOPoint = node;
+        Node fastPoint = node;
+        Node middlePoint = null;
+        Node middleLeftPoint = null;
+        Node middleRightPoint = null;
+
+        while (fastPoint.next != null && fastPoint.next.next != null) {
+            slowOPoint = slowOPoint.next;
+            fastPoint = fastPoint.next.next;
+        }
+        if (fastPoint.next != null) {
+            //slowPoint位于中线前一个
+            if (fastPoint.next.next == null) {
+                middleLeftPoint = slowOPoint;
+            }
+            //slowPoint正位于中点
+        } else {
+            middlePoint = slowOPoint;
+        }
+
+//        reverseNodeList();
+
+        return true;
+    }
+
+
+    public static boolean isPalindrome(Node head) {
+        Node current = head;
+        Stack<Node> stack = new Stack();
+        stack.push(current);
+        while (current.next != null) {
+            stack.push(current.next);
+            current = current.next;
+        }
+        current = head;
+        while (!stack.empty()) {
+            if (current.value != stack.pop().value) {
+                return false;
+            }
+            current = current.next;
+        }
+        return true;
+    }
+
+
+    /**
+     * AI生成的,判断链表是否为首尾相接的环
+     *
+     * @param head
+     * @return
+     */
+    public static boolean isCircular(Node head) {
+        if (head == null) {
+            return false;
+        }
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast.next == null || fast.next.next == null) {
+            return false;
+        }
+// 判断是否是头尾相接的圆环
+        Node p = head;
+        while (p != fast) {
+            p = p.next;
+            fast = fast.next;
+        }
+        return p == head;
+    }
+
+    /**
+     * 判断一个链表是否为环
+     * 如果非环返回null
+     * 如果为环返回入环节点(考虑到了首尾相接)
+     * @param head
+     * @return
+     */
+    public static Node getEnterCircleNode(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node fastP = head;
+        Node slowP = head;
+        while (fastP != null && slowP != null && fastP.next != null) {
+            fastP = fastP.next.next;
+            slowP = slowP.next;
+            if (fastP == slowP) {
+                break;
+            }
+        }
+//        有环
+        if (fastP == slowP) {
+            //首尾相接
+            if (fastP == head) {
+                return head;
+            } else {
+                //不是首尾相接
+                fastP = head;
+                while (fastP != null && slowP != null) {
+                    fastP = fastP.next;
+                    slowP = slowP.next;
+                    if (fastP == slowP) {
+                        return fastP;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 打破环
+     *
+     * @param head
+     * @param enterCircleNode
+     * @return
+     */
+    public static Node breakCircle(Node head, Node enterCircleNode) {
+        boolean hasAppear = false;
+        while (head != null && head.next != null && enterCircleNode != null) {
+            Node curNode = head;
+            while (curNode != null && curNode.next != null) {
+                if (curNode.next == enterCircleNode) {
+                    if (!hasAppear) {
+                        hasAppear = true;
+                    } else {
+                        curNode.next = null;
+                        return head;
+                    }
+                }
+                curNode = curNode.next;
+            }
+
+        }
+        return null;
+    }
+
+    /**
+     * 继续维持环结构
+     * @param head
+     * @param enterNode
+     * @return
+     */
+    public static boolean continueCircle(Node head,Node enterNode){
+        Node current = head;
+        while (current!=null){
+            if(current.next == null){
+                current.next = enterNode;
+                return  true;
+            }
+            current = current.next;
+        }
+        return false;
     }
 }
--- java_project/src/myjava/day8/MainDay8.java
+++ java_project/src/myjava/day8/MainDay8.java
@@ -7,20 +7,79 @@ import java.util.Arrays;
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class MainDay8 {
- public static    int[] array={1,52,5,234,23,1324,876,8678,5,67,456,345,345,34,56,4526,5,54,3425,435,45,34,52,1,4,4,56,67,457,56,34,5,3456};
- public static    int[] array2={8,6,5,5,5,4,3,2};
- public static    int[] array3={8,6,5,1,5,4,3,2};
+    public static int[] array = {1, 52, 5, 234, 23, 1324, 876, 8678, 5, 67, 456, 345, 345, 34, 56, 4526, 5, 54, 3425, 435, 45, 34, 52, 1, 4, 4, 56, 67, 457, 56, 34, 5, 3456};
+    public static int[] array2 = {8, 6, 5, 5, 5, 4, 3, 2};
+    public static int[] array3 = {8, 6, 5, 1, 5, 4, 3, 2};
 
     public static void main(String[] args) {
-//        Node nodeByValue3 = Day8.createSingleNodeByValue(3);
-//        Day8.printNode(nodeByValue3);
-//        Node reversedNodeList = Day8.reverseNodeList(nodeByValue3);
-//        Day8.printNode(reversedNodeList);
+//        Node doubleNodeByValue = Day8.createDoubleNodeByValue(10);
+//        Day8.printNode(doubleNodeByValue);
+//        Node reversDoubleNodeList = Day8.reversDoubleNodeList(doubleNodeByValue);
+//        Day8.printNode(reversDoubleNodeList);
 
-        Node doubleNodeByValue = Day8.createDoubleNodeByValue(3);
-        Day8.printNode(doubleNodeByValue);
-        Node reversDoubleNodeList = Day8.reversDoubleNodeList(doubleNodeByValue);
-        Day8.printNode(reversDoubleNodeList);
+        Node node0 = new Node(0);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node4_3 = new Node(3);
+        Node node5_2 = new Node(2);
+        Node node6_1 = new Node(1);
+        Node node7_0 = new Node(0);
+
+//        node0.next = node1;
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4_3;
+//        node4_3.next = node5_2;
+//        node5_2.next = node6_1;
+//        node6_1.next = node7_0;
+//        node7_0.next = node0;
+//        if (Day8.isPalindrome(node0)) {
+//            System.out.println("是回文");
+//        } else {
+//            System.out.println("不是回文");
+//        }
+
+//判断是否头尾相接
+//
+//        node0.next = node1;
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node0;
+//        if (Day8.isCircular(node0)) {
+//            System.out.println("是头尾相接");
+//        } else {
+//            System.out.println("不是头尾相接");
+//        }
+
+
+
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node0;
+    //返回该链表入环node,如果不是环,返回null
+    if(Day8.getEnterCircleNode(node0)!=null){
+        Node enterNode = Day8.getEnterCircleNode(node0);
+        System.out.println("是环,入环节点值为"+enterNode.value);
+        Node headNode = Day8.breakCircle(node0,enterNode);
+        Day8.printNode(node0);
+        Day8.continueCircle(node0,enterNode);
+        if(Day8.getEnterCircleNode(node0)!=null){
+             enterNode = Day8.getEnterCircleNode(node0);
+            System.out.println("是环,入环节点值为"+enterNode.value);
+        }else {
+            System.out.println("非环");
+
+        }
+
+    }    else {
+            System.out.println("非环");
+
+    }
 
 //       System.out.println(Arrays.toString(Day4.partition(array,0,array.length-1)));
 //       System.out.println(Arrays.toString(Day7.heapSort(array)));
